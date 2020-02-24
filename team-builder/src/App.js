af17c1ecaa0224de2fca8d0e6b9e7b342eb64ff2
Courtney Buratto <cmburatto@gmail.com>
2020-02-24 15:02:20 -0700

    refactored onChange

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+	const [formValues, setFormValues] = useState({ name: ' ' })
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [role, setRole] = useState('role')
+
+	const onChange = e => {
+		setFormValues({
+			...formValues,
+			[e.targe.name]: e.target.value
+		})
+	}
 	return (
 		<div className='App'>
-			<form>
+			<form onChange={onChange}>
 				<label>
-					Name: <input id='name' type='text'></input>
+					Name:
+					<input id='name' type='text'></input>
 				</label>
 				<label>
-					Email: <input id='email' type='email'></input>
+					Email:
+					<input id='email' type='email'></input>
 				</label>
 				<label>
-					Role: <input id='role' type='text'></input>
+					Role:
+					<input id='role' type='text'></input>
 				</label>
 				<input type='submit'></input>
 			</form>
