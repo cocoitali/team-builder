a57738af4476fa3f35fe5bff49e05b1cc646d219
Courtney Buratto <cmburatto@gmail.com>
2020-02-25 10:03:49 -0700

    Set onChange to control entire form, added placeholders

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-	const [formValues, setFormValues] = useState({ name: ' ' })
-	const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
-	const [role, setRole] = useState('role')
+	const [formValues, setFormValues] = useState({ name: ' ', email: ' ', role: ' '  })
+	// const [name, setName] = useState('')
+	// const [email, setEmail] = useState('')
+	// const [role, setRole] = useState('role')
 
 	const onChange = e => {
 		setFormValues({
@@ -18,15 +18,15 @@ function App() {
 			<form onChange={onChange}>
 				<label>
 					Name:
-					<input id='name' type='text'></input>
+					<input id='name' placeholder= 'enter name'type='text'></input>
 				</label>
 				<label>
 					Email:
-					<input id='email' type='email'></input>
+					<input id='email' placeholder= 'enter email' type='email'></input>
 				</label>
 				<label>
 					Role:
-					<input id='role' type='text'></input>
+					<input id='role' placeholder= 'what is your role?' type='text'></input>
 				</label>
 				<input type='submit'></input>
 			</form>
